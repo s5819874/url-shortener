2caf6e1dc2f024d96288c9f53774fbc5444cafef
Po <s5819874@hotmail.com>
2021-02-28 10:44:01 +0800

    feat: add body-parser

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/url'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/url-shortener'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
